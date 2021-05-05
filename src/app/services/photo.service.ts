fa2d4536a92303c1c9fcfe4379e3e0deebe407f6
Damien <damvdc@gmail.com>
2021-05-05 14:55:13 +0200

    initial commit

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+// Import Capacitor dependencies and get references to the Camera, Filesystem, and Storage plugins
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory,
+         CameraPhoto, CameraSource } from '@capacitor/core';
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  public photos: Photo[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+
+  }
+}
+
+export interface Photo {
+  filepath: string;
+  webviewPath: string;
+}
